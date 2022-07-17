21933d8eb0b8bc8b29119441b9420732dd4dfce6
Jovan <40567091+jxmked@users.noreply.github.com>
2022-07-17 13:11:49 +0800

    update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-#For personal use only.
+# Developed by Jovan De Guia.
+# License Under MIT License
 
 #Downloaded files will go here
 _FOLDER='/storage/emulated/0/@webpage/git clone'
+
 _DATAFOLDER=".xio"
+
+TOKEN="<Your Github Token else cloning with data and private repo will not work>"
 
 #Temporary folder
 TMP="tmp"
@@ -12,11 +16,107 @@ TMP="tmp"
 USERNAME=""
 REPO=""
 
-#Check for Parameter url
+# ----------------------------------------
+# Do we have a parameter?
+
 if [[ ! $1 ]]; then
     echo "No parameter found"
     exit 1
 fi
+
+# ----------------------------------------
+# Get URL Index from arguments
+function getURLIndex(){
+    local args=$@
+    local index=0
+    
+    # Any Valid URL
+    # https://stackoverflow.com/questions/3183444/check-for-valid-link-url
+    #local regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
+    
+    # Github URL ~ Modified Slightly
+    # https://serverfault.com/questions/417241/extract-repository-name-from-github-url-in-bash
+    local regex='^(https|git)(:\/\/|@)?(github)([^\/:]+)[\/:]([^\/:]+)\/(.+)(.git)?$'
+    
+    for i in ${args[@]}; do
+        if [[ "${i}" =~ $regex ]]; then
+            echo $index
+            break
+        fi
+        ((index++))
+    done
+}
+
+# ----------------------------------------
+# Re-create parameters
+params=()
+
+for arg in ${@}; do
+    params+=($arg)
+done
+# ----------------------------------------
+# Get Github URL index
+index=$(getURLIndex ${params[@]}) # Index
+
+if [[ ! $index ]]; then
+    echo "No Valid Github URL Found"
+    exit 1
+fi
+
+_REPO=("${params[$index]}") # REPO
+
+# ----------------------------------------
+# Pop valid url from Parameters
+new_arr=()
+for val in ${params[@]}; do
+    if [[ ! "${val}" == "${_REPO}" ]]; then
+        new_arr+=($val)
+    fi
+done
+
+_ARGS=( "${new_arr[@]}" )
+unset new_arr
+
+# I Re-create the array to make sure their is no gap
+
+# ----------------------------------------
+
+# Build the function if have a valid Url
+# For Faster Performance
+
+function getFromArgs(){
+    # @param 1st index -> Target
+    # @param[] 2nd... index -> Search from array
+    local KEY=$1
+    set -- "${@:2}" # Remove First Index
+    
+    local found=0
+    
+    for V in ${@}; do
+        if [[ "${V}" == "${KEY}" ]]; then
+            found=1
+            break
+        fi
+    done
+    echo $found
+}
+
+# ----------------------------------------
+# Get Flags if exists and change state
+
+IS_PRIVATE=0
+WITH_DATA=0
+
+if [[ $_ARGS ]]; then
+    # If We Need Tk Clone Private Repository
+    # We need its `.git` data. It is required
+    IS_PRIVATE=$(getFromArgs '-p' ${_ARGS[@]})
+    
+    # Need `.git` folder?
+    WITH_DATA=$(getFromArgs '-w' ${_ARGS[@]})
+fi
+
+# ----------------------------------------
 
 function createDir(){
     #Create if when not exists
@@ -42,41 +142,49 @@ function checkExistence(){
 BRANCH="master" #Default
 TR=""
 
-if [[ $(echo $1 | cut -d'/' -f 1) == 'https:' ]]; then 
-    USERNAME=$(echo $1 | cut -d'/' -f 4)
-    REPO=$(echo $1 | cut -d'/' -f 5)
+if [[ $(echo $_REPO | cut -d'/' -f 1) == 'https:' ]]; then 
+    USERNAME=$(echo $_REPO | cut -d'/' -f 4)
+    REPO=$(echo $_REPO | cut -d'/' -f 5)
     
-    TR=$(echo $1 | cut -d'/' -f 6)
+    TR=$(echo $_REPO | cut -d'/' -f 6)
     
     #Check for branch
     if [[ $TR == "tree" ]]; then
         
         cn=-1
-        IFS='/' read -ra TMPB <<< "$1"
+        IFS='/' read -ra TMPB <<< "$_REPO"
         
         for i in "${TMPB[@]}"; do
             cn=$(($cn + ${#i} + 1))
             if [[ $TR == $i ]]; then
-                BRANCH=${1:($cn + 1)}
+                BRANCH=${_REPO:($cn + 1)}
                 break
             fi
         done
     fi
 else
-    USERNAME=$(echo $1 | cut -d'/' -f 1)
-    REPO=$(echo $1 | cut -d'/' -f 2)
+    USERNAME=$(echo $_REPO | cut -d'/' -f 1)
+    REPO=$(echo $_REPO | cut -d'/' -f 2)
 fi
 
 RES="${USERNAME}/${REPO}"
 
+MM='Cloning...'
+
 #Check for existence of Github Repo
 if ! curl --output /dev/null --silent --head --fail "https://github.com/${RES}"; then
-    echo -e "\e[1;31mFailed: https://github.com/${RES} does not exists.\e[0m"
-    echo "Exiting..."
-    exit 1 #Terminate
+    #If were cloning private repository, the code above will fail
+    if [[ $WITH_DATA -eq 0 && $IS_PRIVATE -eq 0 ]]; then
+        echo -e "\e[1;31mFailed: https://github.com/${RES} does not exists.\e[0m"
+        echo "Maybe it is a private repository."
+        echo "Use 'clone.sh %url% -w -p' to clone" 
+        echo "Exiting..."
+        exit 1 #Terminate
+    fi
+    MM='Cloning private repository...'
 fi
 
-echo "Cloning..."
+echo $MM
 printf "\n"
 
 cd "${_FOLDER}"
@@ -99,15 +207,13 @@ createDir "${_FOLDER}/${USERNAME}/${OUTPUT}"
 cd "${_FOLDER}/${_DATAFOLDER}/${TMP}"
     
 #Just enter anything from param 2 to clone with data
-if [[ $2 == '-w' ]]; then
+if [[ $WITH_DATA -eq 1 ]]; then
     #When cloning with data
-    TOKEN=<TOKEN HERE>
-    
     if [[ $TR == 'tree' ]]; then
         git clone --single-branch --branch $BRANCH https://${TOKEN}@github.com/$RES.git || { onError 1; }
     else
         #Without defined branch
-        git clone "https://${TOKEN}@${1:8}.git" || { onError 1; }
+        git clone "https://${TOKEN}@${_REPO:8}.git" || { onError 1; }
     fi
     
     MSG="\e[1;32mCloned with data\e[0m"
@@ -140,13 +246,19 @@ echo -e $MSG
 #User > Branch > Repository
 echo "$(date) | $USERNAME > $BRANCH > $REPO" >> "$_FOLDER/${_DATAFOLDER}/log.txt"
 
+
+echo
+
 #printf "Downloaded content size: "
 #du -hs "${_FOLDER}/${RES}"
 
 echo $USERNAME" > "$REPO
+
+echo "Directory: ${_FOLDER}/${USERNAME}/${OUTPUT}"
+
 printf "\n"
 
-#Developed with Love and Frustration by Jovan
-#For Personal Use Only
-#License under MIT
-#Model:SH-CSA-0002 - Advance Repository Cloner API
+# Developed with Love and Frustration by Jovan De Guia
+# License under MIT License
+# Github Username: jxmked
+#Model:SH-CSA-0005 - Advance Repository Cloner API
