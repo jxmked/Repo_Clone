4debb064bd324b078ec40a1d7e7df5f034d62e04
jxmked <jovandeguia@gmail.com>
2024-09-07 21:35:54 +0800

    tabsize:2

--- src/util.rs
+++ src/util.rs
@@ -5,16 +5,17 @@ use std::path::Path;
 
 // https://play.rust-lang.org/?version=stable&mode=debug&edition=2015&gist=1434052276de34362138cba939f6967a
 pub fn not<T: Not>(x: T) -> <T as Not>::Output {
-    Not::not(x)
+  Not::not(x)
 }
 
 pub fn read_file(file_path: &str) -> String {
-
   create_if_not_exists(file_path);
 
   let mut file = File::open(file_path).unwrap();
   let mut contents = String::new();
-  file.read_to_string(&mut contents).expect("Failed to read file");
+  file
+    .read_to_string(&mut contents)
+    .expect("Failed to read file");
 
   return contents;
 }
@@ -23,15 +24,17 @@ pub fn write_file(file_path: &str, contents: &str) {
   create_if_not_exists(file_path);
 
   let mut file = File::create(file_path).unwrap();
-  file.write_all(contents.as_bytes()).expect("Failed to write file");
+  file
+    .write_all(contents.as_bytes())
+    .expect("Failed to write file");
 }
 
 pub fn create_if_not_exists(file_path: &str) {
   if !file_exists(file_path) {
-      File::create(file_path).expect("Failed to create file");
+    File::create(file_path).expect("Failed to create file");
   }
 }
 
-pub fn file_exists(file_path: &str ) -> bool {
+pub fn file_exists(file_path: &str) -> bool {
   Path::new(file_path).is_file()
 }
