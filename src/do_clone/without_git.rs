49c7fd2a75e5f34b29752b62356852a50bdae947
jxmked <jovandeguia@gmail.com>
2024-09-16 20:06:05 +0800

    update

--- src/do_clone/without_git.rs
+++ src/do_clone/without_git.rs
@@ -12,7 +12,6 @@ use crate::constants;
 use crate::git_url_destructor::GitUrlDestructor;
 
 pub async fn without_git(data: GitUrlDestructor, output_dir: &str) -> Result<(), reqwest::Error> {
-
   // We have a problem...
   // If the main branch has different names and the master/main
   // branch remains as sub branch...
@@ -22,23 +21,36 @@ pub async fn without_git(data: GitUrlDestructor, output_dir: &str) -> Result<(),
     constants::MASTER_BRANCH.to_string()
   };
 
-  let url = format!(
+  let url: String = format!(
     "https://github.com/{}/{}/tarball/{}",
     data.username, data.repository, branch
   );
 
-  let resp = reqwest::get(url).into_future().await.unwrap();
+  // Setting up temp folder
+  let root_exe = std::env::current_exe().unwrap();
+  let root_exe_temp_folder = constants::GZIP_TEMP_FOLDER;
+
+  // hahahaha
+  let exe_root = Path::new(&root_exe);
+  let exe_root = &exe_root.join(&root_exe_temp_folder);
+  let exe_root = &exe_root.join("temp.tar.gz");
+
+  // Setting final output folder
+  
+
+  // Creating connection and downloading repo
+  let resp: reqwest::Response = reqwest::get(url).into_future().await.unwrap();
+  let mut u8_bytes: &[u8] = &resp.bytes().await.unwrap();
 
   let x_path = Path::new(&output_dir);
 
   let efile = &x_path.join("samp.tar.gz");
 
-  let mut out = File::create(efile).expect("failed to create file");
+  let mut out = File::create(exe_root).expect("failed to create file");
 
-  let mut ep: &[u8] = &resp.bytes().await.unwrap();
-  std::io::copy(&mut ep, &mut out).expect("failed to copy content");
+  std::io::copy(&mut u8_bytes, &mut out).expect("failed to copy content");
 
-  let gg = File::open(efile).unwrap();
+  let gg = File::open(exe_root).unwrap();
 
   let tar = GzDecoder::new(gg);
   let mut archive = Archive::new(tar);
